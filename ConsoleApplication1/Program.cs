2ee6af4fe21f8b9d15d614afaf2e6a59a6835de8
L00068270 <L00068270@student.lyit.ie>
2018-08-31 15:20:08 +0100

    Commit

--- ConsoleApplication1/Program.cs
+++ ConsoleApplication1/Program.cs
@@ -8,8 +8,32 @@ namespace ConsoleApplication1
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
+            Console.WriteLine("Please enter a number");
+            int userNumber = int.Parse(Console.ReadLine());
+
+            if (userNumber == 1)
+            {
+                Console.WriteLine("Your Number is 1");              
+            }
+            if (userNumber == 2)
+            {
+                Console.WriteLine("Your Number is 2");                
+            }
+            if (userNumber == 3)
+            {
+                Console.WriteLine("Your Number is 3");                
+            }
+            if (userNumber == 4)
+            {
+                Console.WriteLine("Your Number is 4");                
+            }
+            if (userNumber != 1 && userNumber != 2 && userNumber !=3 && userNumber != 4)
+            {
+                Console.WriteLine("Please pick a number between 1 and 4");              
+            }
+            Console.Read();
         }
     }
 }
